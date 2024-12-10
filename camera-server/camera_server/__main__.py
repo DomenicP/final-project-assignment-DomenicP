60289089ec3517a128bda2b3851d4b66ba0a6a97
Domenic Rodriguez <domenicpaul@me.com>
2024-12-10 13:38:46 -0500

    Add stream management to camera-server

--- camera-server/camera_server/__main__.py
+++ camera-server/camera_server/__main__.py
@@ -1,8 +1,24 @@
 """Camera server application."""
 
 import json
+import shlex
+import subprocess
 
 import zmq
+
+
+PIPELINES = {
+    "mystream": [
+        "v4l2src",
+        "video/x-raw,width=640,height=480",
+        "nvvidconv",
+        "nvv4l2h264enc",
+        "queue",
+        "rtspclientsink location=rtsp://localhost:8554/mystream latency=0",
+    ]
+}
+
+streams = {}
 
 
 def main():
@@ -16,8 +32,55 @@ def main():
     while True:
         msg_str = socket.recv_string()
         topic, data_str = msg_str.split(" ", 1)
-        data = json.loads(data_str)
-        print(f"Received message: topic={topic} data={data}")
+        value = json.loads(data_str)
+        print(f"Received message: topic={topic} value={value}")
+
+        camera_id, subtopic = topic.split("/")[2:4]
+        if camera_id not in PIPELINES:
+            print(f"Unknown stream {camera_id}")
+            continue
+
+        if subtopic == "enabled":
+            enabled = value
+            if enabled:
+                start_stream(camera_id)
+            else:
+                stop_stream(camera_id)
+        else:
+            print(f"Unknown subtopic {subtopic}")
+            continue
+
+
+def start_stream(camera_id):
+    global streams
+    if camera_id in streams:
+        print(f"Camera stream {camera_id} is already running")
+    else:
+        cmd = shlex.split(f"gst-launch-1.0 {' ! '.join(PIPELINES[camera_id])}")
+        print(f"Starting camera pipeline for {camera_id}")
+        print(cmd)
+        streams[camera_id] = subprocess.Popen(cmd)
+
+
+def stop_stream(camera_id):
+    global streams
+    if camera_id in streams:
+        try_shutdown(camera_id)
+        del streams[camera_id]
+    else:
+        print(f"Camera stream {camera_id} is not running")
+
+
+def try_shutdown(camera_id):
+    global streams
+    print(f"Terminating pipeline for {camera_id}")
+    streams[camera_id].terminate()
+    try:
+        streams[camera_id].wait(5)
+    except subprocess.TimeoutExpired:
+        print("ERROR: timed out waiting for pipeline to terminate")
+    else:
+        print("Stream terminated cleanly")
 
 
 if __name__ == "__main__":
@@ -25,3 +88,5 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("Shutting down")
+    for stream in streams:
+        try_shutdown(stream)
